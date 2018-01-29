f9664e970bc6f1354bf18a283fe905a01d79932e
noal.jiang <781910469@qq.com>
2018-01-29 19:38:51 +0800

    修复后台

--- .history/public/javascripts/common_20180129192551.js
+++ .history/public/javascripts/common_20180129192551.js
@@ -1,3 +1,4 @@
-var appUrl = 'http://120.78.51.235:80';
+
+var appUrl = 'http://120.78.51.235:3389';
 // var appUrl = 'http://localhost:3389';
 FastClick.attach(document.body);
--- .history/public/javascripts/common_20180129193823.js
+++ .history/public/javascripts/common_20180129193823.js
@@ -1,3 +1,3 @@
-var appUrl = 'http://120.78.51.235:80';
+var appUrl = 'http://120.78.51.235:8';
 // var appUrl = 'http://localhost:3389';
 FastClick.attach(document.body);
